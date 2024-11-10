944b27512818e4f371b4ca7755acfcacc6724db6
Amine <aminemannai13@gmail.com>
2024-11-11 00:28:26 +0100

    feat: adding bootstrap 4, fontawesome and meta data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <>test</>;
+  return (
+    <div className="container">
+      <h1 className="text-primary">Hello, Bootstrap 4</h1>
+      <button className="btn btn-primary">
+        <i className="fa-regular fa-heart"></i> Like
+      </button>
+    </div>
+  );
 }
 
 export default App;
